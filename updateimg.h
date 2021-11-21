b2338ac1cb5a73b781c4760e4df8cc1b054300db
SNC6SI <shingosch@hotmail.com>
2021-11-21 21:20:20 +0800

    update macro of colors

--- updateimg.h
+++ updateimg.h
@@ -3,9 +3,10 @@
 
 #include<opencv2/opencv.hpp>
 
-#define BLUE Scalar(0, 0, 255)
-#define RED  Scalar(255, 0, 0)
+#define BLUE   Scalar(0, 0, 255)
+#define RED    Scalar(255, 0, 0)
 #define BLACK  Scalar(0, 0, 0)
+#define GRAY   Scalar(224,224,224)
 
 #define XCOL 1600
 #define YROW 1000
