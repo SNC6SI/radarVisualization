592c26b02d171151dd85501ff049e30bf8ba2367
SNC6SI <shingosch@hotmail.com>
2021-12-12 21:22:38 +0800

    add parse map objs' type/prob/heightprob

---
+++ m/upack_config_6.m
@@ -0,0 +1,78 @@
+function config = upack_config_6
+    config = {...
+        0x172, 'MapObj01Type';
+        0x172, 'MapObj02Type';
+        0x172, 'MapObj03Type';
+        0x172, 'MapObj04Type';
+
+        0x172, 'MapObj01Prob';
+        0x172, 'MapObj02Prob';
+        0x172, 'MapObj03Prob';
+        0x172, 'MapObj04Prob';
+
+        0x172, 'MapObj01HeightProb';
+        0x172, 'MapObj02HeightProb';
+        0x172, 'MapObj03HeightProb';
+        0x172, 'MapObj04HeightProb';
+
+        0x173, 'MapObj05Type';
+        0x173, 'MapObj06Type';
+        0x173, 'MapObj07Type';
+        0x173, 'MapObj08Type';
+
+        0x173, 'MapObj05Prob';
+        0x173, 'MapObj06Prob';
+        0x173, 'MapObj07Prob';
+        0x173, 'MapObj08Prob';
+
+        0x173, 'MapObj05HeightProb';
+        0x173, 'MapObj06HeightProb';
+        0x173, 'MapObj07HeightProb';
+        0x173, 'MapObj08HeightProb';
+
+        0x174, 'MapObj09Type';
+        0x174, 'MapObj10Type';
+        0x174, 'MapObj11Type';
+        0x174, 'MapObj12Type';
+
+        0x174, 'MapObj09Prob';
+        0x174, 'MapObj10Prob';
+        0x174, 'MapObj11Prob';
+        0x174, 'MapObj12Prob';
+
+        0x174, 'MapObj09HeightProb';
+        0x174, 'MapObj10HeightProb';
+        0x174, 'MapObj11HeightProb';
+        0x174, 'MapObj12HeightProb';
+
+        0x177, 'MapObj13Type';
+        0x177, 'MapObj14Type';
+        0x177, 'MapObj15Type';
+        0x177, 'MapObj16Type';
+
+        0x177, 'MapObj13Prob';
+        0x177, 'MapObj14Prob';
+        0x177, 'MapObj15Prob';
+        0x177, 'MapObj16Prob';
+
+        0x177, 'MapObj13HeightProb';
+        0x177, 'MapObj14HeightProb';
+        0x177, 'MapObj15HeightProb';
+        0x177, 'MapObj16HeightProb';
+        
+        0x178, 'MapObj17Type';
+        0x178, 'MapObj18Type';
+        0x178, 'MapObj19Type';
+        0x178, 'MapObj20Type';
+
+        0x178, 'MapObj17Prob';
+        0x178, 'MapObj18Prob';
+        0x178, 'MapObj19Prob';
+        0x178, 'MapObj20Prob';
+
+        0x178, 'MapObj17HeightProb';
+        0x178, 'MapObj18HeightProb';
+        0x178, 'MapObj19HeightProb';
+        0x178, 'MapObj20HeightProb';
+        };
+end
--- m/write_unpack_code.m
+++ m/write_unpack_code.m
@@ -4,7 +4,7 @@ function [msg_unpack_s, def_s, dcl_s] = write_unpack_code
 
     % read config: canid, signal_name
 %     config = upack_config;
-    config = upack_config_5;
+    config = upack_config_6;
 
     % 
     unique_canids = unique(cell2mat(config(:,1)));
