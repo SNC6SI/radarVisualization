ed1385854367894e9843525653cbef796528b53c
SNC6SI <shingosch@hotmail.com>
2021-12-12 21:50:52 +0800

    add parse slot type and slot obj type

---
+++ m/upack_config_7.m
@@ -0,0 +1,13 @@
+function config = upack_config_7
+    config = {...
+        0x179, 'ApaPscLeftSlot0Tpye';
+        0x179, 'ApaPscLeftSlot1Tpye';
+        0x179, 'ApaPsc_LeftPSL0_ObjectType';
+        0x179, 'ApaPsc_LeftPSL1_ObjectType';
+
+        0x17A, 'ApaPscRightSlot0Tpye';
+        0x17A, 'ApaPscRightSlot1Tpye';
+        0x17A, 'ApaPsc_RightPSL0_ObjectType';
+        0x17A, 'ApaPsc_RightPSL1_ObjectType';
+        };
+end
